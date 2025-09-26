4b4bcffc7fae3a25d3119b26929e161f14384350
VedDhanokar <veddhanokat@gmail.com>
2025-09-26 16:24:29 +0530

    Delete script directory

--- script/main.py
+++
@@ -1,7 +0,0 @@
-# main.py
-from preprocessing import preprocess_pipeline
-
-df_cleaned = preprocess_pipeline(
-    input_path="D:\Subjects_Languages\Languages\ML_Internship\Dataset\titanic.csv",
-    output_path="D:\Subjects_Languages\Languages\ML_Internship\Output"
-)
